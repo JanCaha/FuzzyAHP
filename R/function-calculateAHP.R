51542d2ab659ec3c07357aa8a7177f11e954d1b6
Jan Caha <cahik@atlas.cz>
2016-03-22 13:32:10 +0100

    added two new variants of the function-calculateAHP.R to calculate directly with comparison matrixes instead of weights
    Signed-off-by: Jan Caha <cahik@atlas.cz>

--- R/function-calculateAHP.R
+++ R/function-calculateAHP.R
@@ -4,7 +4,10 @@
 #' or \code{\linkS4class{FuzzyWeights}} on data represented either by \code{matrix} or
 #' \code{\linkS4class{FuzzyData}}.
 #'
-#' @param weights object of class \linkS4class{Weights} or \linkS4class{FuzzyWeights}.
+#' @param weights object of class \linkS4class{Weights} or \linkS4class{FuzzyWeights}. Alternatively objects of
+#' classes \linkS4class{PairwiseComparisonMatrix} or \linkS4class{FuzzyPairwiseComparisonMatrix} can be passed to
+#' directly calculate weights from these classes.
+#'
 #' @param data matrix or \linkS4class{FuzzyData} with number of colums equal to number of rows in \code{weights}.
 #'
 #' @return Either a matrix (if \linkS4class{Weights} and \code{matrix} were used as inputs) or
@@ -23,27 +26,15 @@ setMethod(
       stop("Can not multiply the data by the weights, the number of data columns does not match the number of weights.")
     }
 
-    # pb = txtProgressBar(min=0, max=1, initial = 0, style = 3)
-
     numberRows = nrow(data)
 
     result = matrix(nrow = numberRows, ncol = 1)
 
     result[, 1] = rowSums(t(t(data[,]) * weights@weights))
 
-#       for (i in 1:nrow(data)){
-#
-#         if (any(is.na(data[i,]))){
-#           result[i, 1] = NA
-#         } else {
-#           result[i,1] = sum(data[i,] * w@weights)
-#         }
-#
-#         setTxtProgressBar(pb, ((i/numberRows)))
-#       }
+    colnames(result) = c("result")
 
-      colnames(result) = c("result")
-      return(result)
+    return(result)
   }
 )
 
@@ -56,8 +47,6 @@ setMethod(
       stop("Can not multiply the data by the weights, the number of data columns does not match the number of weights.")
     }
 
-    # pb = txtProgressBar(min=0, max=1, initial = 0, style = 3)
-
     numberRows = nrow(data)
 
     result = matrix(nrow = numberRows, ncol = 3)
@@ -66,30 +55,6 @@ setMethod(
     result[, 2] = rowSums(t(t(data[,]) * weights@fnModal))
     result[, 3] = rowSums(t(t(data[,]) * weights@fnMax))
 
-#     for (i in 1:nrow(data)){
-#
-#       if (any(is.na(data[i,]))){
-#         result[i, 1] = NA
-#         result[i, 2] = NA
-#         result[i, 3] = NA
-#
-#       }else{
-#
-#         result[i, 1] = sum(data[i,] * weights@weights[,1])
-#         result[i, 2] = sum(data[i,] * weights@weights[,2])
-#         result[i, 3] = sum(data[i,] * weights@weights[,3])
-#       }
-#
-#       #if(i%%10000 == 0){
-#       #  print(paste("Calculated ", (i/numberRows)*100, "% of data."))
-#       #}
-#
-#       setTxtProgressBar(pb, ((i/numberRows)))
-#
-#     }
-#     close(pb)
-
-    #colnames(result) = c("result_minimum", "result_modal", "result_maximum")
     return(new("FuzzyData", fnMin = as.matrix(result[,1]), fnModal = as.matrix(result[,2]), fnMax = as.matrix(result[,3])))
   }
 )
@@ -103,8 +68,6 @@ setMethod(
       stop("Can not multiply the fuzzy data by the weights, the number of data columns does not match the number of weights.")
     }
 
-    # pb = txtProgressBar(min=0, max=1, initial = 0, style = 3)
-
     numberRows = nrow(data@fnModal)
 
     result = matrix(nrow = numberRows, ncol = 3)
@@ -113,24 +76,24 @@ setMethod(
     result[, 2] = rowSums(t(t(data@fnModal[,]) * weights@fnModal))
     result[, 3] = rowSums(t(t(data@fnMax[,]) * weights@fnMax))
 
-#     for (i in 1:nrow(data)){
-#
-#       if (any(is.na(data[i,]))){
-#         result[i, 1] = NA
-#         result[i, 2] = NA
-#         result[i, 3] = NA
-#
-#       }else{
-#
-#         result[i, 1] = sum(data@fnMin[i,] * weights@weights[,1])
-#         result[i, 2] = sum(data@fnModal[i,] * weights@weights[,2])
-#         result[i, 3] = sum(data@fnMax[i,] * weights@weights[,3])
-#       }
-#
-#       setTxtProgressBar(pb, ((i/numberRows)))
-#     }
-
-    #colnames(result) = c("result_minimum", "result_modal", "result_maximum")
     return(new("FuzzyData", fnMin = as.matrix(result[,1]), fnModal = as.matrix(result[,2]), fnMax = as.matrix(result[,3])))
+  }
+)
+
+setMethod(
+  f="calculateAHP",
+  signature(weights = "PairwiseComparisonMatrix", data = "matrix"),
+  definition=function(weights, data)
+  {
+    return (calculateAHP(calculateWeights(weights), data))
+  }
+)
+
+setMethod(
+  f="calculateAHP",
+  signature(weights = "FuzzyPairwiseComparisonMatrix", data = "matrix"),
+  definition=function(weights, data)
+  {
+    return (calculateAHP(calculateWeights(weights), data))
   }
 )
