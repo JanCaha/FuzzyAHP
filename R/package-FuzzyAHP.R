0178efb4eec0d0404f6861b6ba2141105dae1df4
Jan Caha <cahik@atlas.cz>
2016-03-23 12:07:40 +0100

    Update to the project, fixed all NOTES from R check
    Signed-off-by: Jan Caha <cahik@atlas.cz>

---
+++ R/package-FuzzyAHP.R
@@ -0,0 +1,26 @@
+#' @title Package for calculation of classic and Fuzzy AHP
+#'
+#' @description
+#' \pkg{FuzzyAHP} is an open source (LGPL 3) package for R.
+#' The package is only suitable for AHP that uses categorical rating of criteria
+#' for alternatives instead of pairwise comparison of alternatives according to
+#' each criteria. This adaptation of AHP is common in situations when the number
+#' of alternatives is hight and the pairwise comparison is thus inadequate or
+#' impossible to construct. The weights for criteria are, however, still determined
+#' from the pairwise comparison matrix. This approach towards AHP is common in
+#' Geosciences as well as other fields.
+#'
+#' @details
+#' Please see vignettes for more details about the package and examples of use.
+#'
+#' Complete list of classes and methods call \code{help(package="FuzzyAHP")}.
+#' \cr\cr
+#'
+#' @name FuzzyAHP-package
+#' @docType package
+#' @import methods
+#' @author Jan Caha \email{jan.caha@@outlook.com},\cr
+#'  with contributions from Aneta Drážná
+#'
+#' @encoding UTF-8
+invisible(NULL)
