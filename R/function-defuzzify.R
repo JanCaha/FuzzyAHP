1644f8de8a2499758107e76f548f3301bedd01a7
Jan Caha <cahik@atlas.cz>
2016-03-24 09:01:05 +0100

    removed old code
    Signed-off-by: Jan Caha <cahik@atlas.cz>

--- R/function-defuzzify.R
+++ R/function-defuzzify.R
@@ -28,8 +28,6 @@ setMethod(
       stop("Defuzzification is only possible for datasets representing one fuzzy number.")
     }
 
-    # pb = txtProgressBar(min=0, max=1, initial = 0, style = 3)
-
     numberRows = nrow(fuzzyData@fnModal)
 
     result = matrix(NA, nrow = numberRows, ncol = 1)
@@ -41,21 +39,6 @@ setMethod(
       result[,1] = (fuzzyData@fnMin[,1] + fuzzyData@fnModal[,1] + fuzzyData@fnMax[,1])/3
     }
 
-#     for(i in 1:numberRows){
-#       if(type == "Chen"){
-#         result[i,1] = (1/2)*(((fuzzyData@fnMax[i,1]-minMin)/((maxMax - minMin)-(fuzzyData@fnModal[i,1]-fuzzyData@fnMax[i,1])))+1-((maxMax-fuzzyData@fnMin[i,1])/((maxMax-minMin)+(fuzzyData@fnModal[i,1]-fuzzyData@fnMin[i,1]))))
-#       }
-#       else if(type == "Yager"){
-#         result[i,1] = (((fuzzyData@fnModal[i,1]-fuzzyData@fnMin[i,1])*(fuzzyData@fnMin[i,1]+(2/3)*(fuzzyData@fnModal[i,1]-fuzzyData@fnMin[i,1]))+((fuzzyData@fnMax[i,1]-fuzzyData@fnModal[i,1])*(fuzzyData@fnModal[i,1]+(1/3)*(fuzzyData@fnMax[i,1]-fuzzyData@fnModal[i,1]))))/((fuzzyData@fnModal[i,1]-fuzzyData@fnMin[i,1])+(fuzzyData@fnMax[i,1]-fuzzyData@fnModal[i,1])))
-#       }
-#       else{
-#         result[i,1] = (fuzzyData@fnMin[i,1] + fuzzyData@fnModal[i,1] + fuzzyData@fnMax[i,1])/3
-#       }
-#
-#       setTxtProgressBar(pb, (i/numberRows))
-#     }
-
     return(result)
-
   }
 )
