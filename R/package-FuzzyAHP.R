1154ae6f90dbf2d13a3ea4cc4b1f55e1e81f411f
Jan Caha <cahik@atlas.cz>
2016-04-01 14:46:08 +0200

    updated CITATION and description of the package
    Signed-off-by: Jan Caha <cahik@atlas.cz>

--- R/package-FuzzyAHP.R
+++ R/package-FuzzyAHP.R
@@ -10,11 +10,18 @@
 #' from the pairwise comparison matrix. This approach towards AHP is common in
 #' Geosciences as well as other fields.
 #'
+#' The determination of criteria weights is done according to process described
+#' by Krejčí, Pavlačka, and Talašová (2016), which yelds significantly narrower
+#' fuzzy numbers than previously used approaches.
+#'
 #' @details
 #' Please see vignettes for more details about the package and examples of use.
 #'
 #' Complete list of classes and methods call \code{help(package="FuzzyAHP")}.
 #' \cr\cr
+#'
+#' @references
+#' Krejčí, Jana, Ondřej Pavlačka, and Jana Talašová. 2016. “A fuzzy extension of Analytic Hierarchy Process based on the constrained fuzzy arithmetic.” Fuzzy Optimization and Decision Making. doi:10.1007/s10700-016-9241-0.
 #'
 #' @name FuzzyAHP-package
 #' @docType package
