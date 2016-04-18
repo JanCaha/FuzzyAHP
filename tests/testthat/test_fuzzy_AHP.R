dae610ba3bb39bd675572226f9f78f8cde5cc7d7
Jan Caha <cahik@atlas.cz>
2016-04-18 10:30:59 +0200

    update of tests
    Signed-off-by: Jan Caha <cahik@atlas.cz>

--- tests/testthat/test_fuzzy_AHP.R
+++ tests/testthat/test_fuzzy_AHP.R
@@ -26,3 +26,32 @@ test_that("Tests of fuzzy AHP calculation", {
   expect_is(fuzzyComparisonMatrix, "FuzzyPairwiseComparisonMatrix")
   expect_is(weights, "FuzzyWeights")
 })
+
+
+test_that("Tests of fuzzy pairwise comparsion matrix consturction from character matrix", {
+
+  matrix = c("1;1;1", "8;9;9", "5;6;7",
+              "1/9;1/9;1/8", "1;1;1", "3;4;5",
+             "1/7;1/6;1/5", "1/5;1/4;1/3", "1;1;1")
+  matrix = matrix(matrix, nrow=3, ncol=3, byrow = TRUE)
+  fuzzyMatrix = fuzzyPairwiseComparisonMatrix(matrix)
+
+  expect_is(fuzzyMatrix, "FuzzyPairwiseComparisonMatrix")
+
+# ------------------------------------------------------------------
+  matrix = c("1;1;1", "8;9;9",
+             "1/9;1/9;1/8", "1;1;1",
+             "1/7;1/6;1/5", "1/5;1/4;1/3")
+  matrix = matrix(matrix, nrow=3, ncol=2, byrow = TRUE)
+
+  expect_error(fuzzyPairwiseComparisonMatrix(matrix), "The fuzzy pairwise comparison matrix is not a square matrix.")
+
+# ------------------------------------------------------------------
+  matrix = c("1;1;1", "8;9;9", "5;6;7",
+             "1/9;1/9;1/8", "1;1;1", "3;4;5",
+             "1/7;1/6;1/5", "1/5;1/4;1/3", "1;1;3")
+  matrix = matrix(matrix, nrow=3, ncol=3, byrow = TRUE)
+
+  expect_error(fuzzyPairwiseComparisonMatrix(matrix), "(1,1,1)")
+  }
+)
--- tests/testthat/test_pairwiseComparisonMatrix.R
+++ tests/testthat/test_pairwiseComparisonMatrix.R
@@ -9,14 +9,15 @@ test_that("Tests of pairwise comparison matrix", {
   expect_is(matrix, "PairwiseComparisonMatrix")
 })
 
-test_that("Tests of pairwise comparison matrix - failed (matrix is numerical)", {
-  comparisonMatrixValues = c(1,9,5,
-                             1/9,1,1/3,
-                             1/5,3,1)
-  comparisonMatrix = matrix(comparisonMatrixValues, nrow = 3, ncol = 3, byrow = TRUE)
-  expect_error(pairwiseComparisonMatrix(comparisonMatrix),
-               "The pairwise comparison matrix needs to be of type character but is double.")
-})
+# since version 0.6.9 it is possible to construct the pairwise comparison matrix event from numerical matrix
+# test_that("Tests of pairwise comparison matrix - failed (matrix is numerical)", {
+#   comparisonMatrixValues = c(1,9,5,
+#                              1/9,1,1/3,
+#                              1/5,3,1)
+#   comparisonMatrix = matrix(comparisonMatrixValues, nrow = 3, ncol = 3, byrow = TRUE)
+#   expect_error(pairwiseComparisonMatrix(comparisonMatrix),
+#                "The pairwise comparison matrix needs to be of type character but is double.")
+# })
 
 test_that("Tests of pairwise comparison matrix - failed (matrix does not have ones on diagonal)", {
   comparisonMatrixValues = c("1","9","5",
